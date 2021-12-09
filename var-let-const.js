c18cd2bcc782b60bd9e228b374474bf028b374f3
Luis-H-Baroni <baroniluishenrique@gmail.com>
2021-12-09 15:35:33 -0300

    diferença entre var,const e let

--- var-let-const.js
+++ var-let-const.js
@@ -1,3 +1,10 @@
+//var tem escopo mais glboal e sem controle, pode alterar valores de variaveis em outros blocos de codigo. não se utiliza.
+var variavel;
+
+//const não permite que mude o valor armazenado. Deve ser declarada e inicializada antes de usar.
+const variavel2;
+
+//let também deve ser declarada
 let forma = "retangulo";
 let altura = 2;
 let comprimento = 5;
